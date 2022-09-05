ab06758ed8ba646585e7b4107ae1bc5ef45b5a1e
Baha Dzhalalov <mckinley666228.o@gmail.com>
2022-09-05 12:04:10 +0300

    change model field parameters and make migrations to db

--- motivator/motivations/models.py
+++ motivator/motivations/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Motivation(models.Model):
-    nickname = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=64, null=True, blank=True)
     motivation = models.TextField(max_length=200)
     is_visible = models.BooleanField(default=True)
